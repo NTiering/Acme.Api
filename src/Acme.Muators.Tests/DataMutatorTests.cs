93155f71b0aaaffcfb8851fadac951b2cf4a10bc
Simon Smith <55135068@ad.mmu.ac.uk>
2020-04-06 10:28:47 +0100

    better names

--- src/Acme.Muators.Tests/DataMutatorTests.cs
+++ src/Acme.Muators.Tests/DataMutatorTests.cs
@@ -19,52 +19,52 @@ namespace Acme.Muators.Tests
         [Fact]
         public async Task ItCallsAddHandlers()
         {
-            var mockAdd = new Mock<IDataMutatorHandler>();
+            var mockhandler = new Mock<IDataMutatorHandler>();
             var mockIdentity = new Mock<IIdentity>();
             var newState = new object();
             var oldState = new object();
 
-            mockAdd.Setup(x => x.CanHandle(It.IsAny<IDataMutatorContext>())).Returns(true);
+            mockhandler.Setup(x => x.CanHandle(It.IsAny<IDataMutatorContext>())).Returns(true);
 
-            await new DataMutator(new[] { mockAdd.Object }).BeforeAdd(newState, mockIdentity.Object);
+            await new DataMutator(new[] { mockhandler.Object }).BeforeAdd(newState, mockIdentity.Object);
 
-            mockAdd.Verify(x => x.BeforeAdd(newState, It.IsAny<IDataMutatorContext>()), Times.Once);
-            mockAdd.Verify(x => x.BeforeModify(oldState, newState, It.IsAny<IDataMutatorContext>()), Times.Never);
-            mockAdd.Verify(x => x.BeforeRemove(oldState, It.IsAny<IDataMutatorContext>()), Times.Never);
+            mockhandler.Verify(x => x.BeforeAdd(newState, It.IsAny<IDataMutatorContext>()), Times.Once);
+            mockhandler.Verify(x => x.BeforeModify(oldState, newState, It.IsAny<IDataMutatorContext>()), Times.Never);
+            mockhandler.Verify(x => x.BeforeRemove(oldState, It.IsAny<IDataMutatorContext>()), Times.Never);
         }
 
         [Fact]
         public async Task ItCallsModifyHandlers()
         {
-            var mockAdd = new Mock<IDataMutatorHandler>();
+            var mockhandler = new Mock<IDataMutatorHandler>();
             var mockIdentity = new Mock<IIdentity>();
             var newState = new object();
             var oldState = new object();
 
-            mockAdd.Setup(x => x.CanHandle(It.IsAny<IDataMutatorContext>())).Returns(true);
+            mockhandler.Setup(x => x.CanHandle(It.IsAny<IDataMutatorContext>())).Returns(true);
 
-            await new DataMutator(new[] { mockAdd.Object }).BeforeModify(oldState, newState, mockIdentity.Object);
+            await new DataMutator(new[] { mockhandler.Object }).BeforeModify(oldState, newState, mockIdentity.Object);
 
-            mockAdd.Verify(x => x.BeforeAdd(newState, It.IsAny<IDataMutatorContext>()), Times.Never);
-            mockAdd.Verify(x => x.BeforeModify(oldState, newState, It.IsAny<IDataMutatorContext>()), Times.Once);
-            mockAdd.Verify(x => x.BeforeRemove(oldState, It.IsAny<IDataMutatorContext>()), Times.Never);
+            mockhandler.Verify(x => x.BeforeAdd(newState, It.IsAny<IDataMutatorContext>()), Times.Never);
+            mockhandler.Verify(x => x.BeforeModify(oldState, newState, It.IsAny<IDataMutatorContext>()), Times.Once);
+            mockhandler.Verify(x => x.BeforeRemove(oldState, It.IsAny<IDataMutatorContext>()), Times.Never);
         }
 
         [Fact]
         public async Task ItCallsRemoveHandlers()
         {
-            var mockAdd = new Mock<IDataMutatorHandler>();
+            var mockhandler = new Mock<IDataMutatorHandler>();
             var mockIdentity = new Mock<IIdentity>();
             var newState = new object();
             var oldState = new object();
 
-            mockAdd.Setup(x => x.CanHandle(It.IsAny<IDataMutatorContext>())).Returns(true);
+            mockhandler.Setup(x => x.CanHandle(It.IsAny<IDataMutatorContext>())).Returns(true);
 
-            await new DataMutator(new[] { mockAdd.Object }).BeforeRemove(oldState, mockIdentity.Object);
+            await new DataMutator(new[] { mockhandler.Object }).BeforeRemove(oldState, mockIdentity.Object);
 
-            mockAdd.Verify(x => x.BeforeAdd(newState, It.IsAny<IDataMutatorContext>()), Times.Never);
-            mockAdd.Verify(x => x.BeforeModify(oldState, newState, It.IsAny<IDataMutatorContext>()), Times.Never);
-            mockAdd.Verify(x => x.BeforeRemove(oldState, It.IsAny<IDataMutatorContext>()), Times.Once);
+            mockhandler.Verify(x => x.BeforeAdd(newState, It.IsAny<IDataMutatorContext>()), Times.Never);
+            mockhandler.Verify(x => x.BeforeModify(oldState, newState, It.IsAny<IDataMutatorContext>()), Times.Never);
+            mockhandler.Verify(x => x.BeforeRemove(oldState, It.IsAny<IDataMutatorContext>()), Times.Once);
         }
 
 
