0ad88c0f61ab726152ac2472f4049110339ffca3
Simon Smith <55135068@ad.mmu.ac.uk>
2020-04-28 11:19:12 +0100

    put cass in own file

---
+++ src/Acme.Web.Api/Ext/ControllerBaseEtx.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Acme.Web.Api.Ext
+{
+    public static class ControllerBaseEtx
+    {
+        public static IActionResult OkOrNotFound(this ControllerBase controller, object o)
+        {
+            if (o == null)
+            {
+                return controller.NotFound();
+            }
+            else
+            {
+                return controller.Ok(o);
+            }
+        }
+
+
+    }
+}
